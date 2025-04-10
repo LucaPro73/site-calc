2486dfce96ae1da34e1da56bf6b53f9af1ad8824
LucaPro73 <206548752+LucaPro73@users.noreply.github.com>
2025-04-10 14:56:40 +0300

    functional

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-document.addEventListener("click", (ev) => {
-    const el = document.getElementById(ev.target.id)
+function addNumber(n, input) {
+    if (input.match(/%$/)) return input;
+    if (!input.match(/\d$/)) return input += ` ${n}`
+    else return input += n;
+}
+function addSymbol(s, input) {
+    if (input.match(/(\d|%)$/)) return input += ` ${s}`;
+    return input;
+}
+function addSecondary(v, input) {
+    if (input.match(/\d$/)) return input += v;
+    return input;
+}
+function erase(input) {
+    if (input.match(/(\d|%)$/))
+        input = input.substring(0, input.length - 1)
+    else input = input.substring(0, input.length - 2)
+    return input;
+}
+/**
+ * @param {string} input 
+ */
+function compute(input) {
+    const i = input.replace(/÷/g, "/").replace(/x/g, "*").split(" ").map(el => {
+        const m = el.match(/([\d\.]+)%/)
+        if (m) return parseInt(m[1]) / 100;
+        else return el
+    }).join(" ")
+    if (input === "9 + 10") return 21;
+    const v = parseFloat(eval(i)).toPrecision(5)
+    const extra = v.match(/\.0+$/)
+    if (extra) return v.substring(0, v.length - extra[0].length)
+    else return v
+}
+
+document.addEventListener("click", (e) => {
+    const el = document.getElementById(e.target.id)
     if (!el) return;
-    console.log(el)
     const input = document.getElementById("input")
     switch (el.id) {
         case "zero":
@@ -14,9 +48,7 @@ document.addEventListener("click", (ev) => {
         case "seven":
         case "eight":
         case "nine":
-            if (input.innerText.match(/%$/)) break;
-            if (!input.innerText.match(/\d$/)) input.innerText += ` ${el.innerText}`
-            else input.innerText += el.innerText;
+            input.innerText = addNumber(el.innerText, input.innerText)
             break;
         case "clear":
             input.innerText = "";
@@ -25,11 +57,11 @@ document.addEventListener("click", (ev) => {
         case "minus":
         case "multiply":
         case "divide":
-            if (input.innerText.match(/(\d|%)$/)) input.innerText += ` ${el.innerText}`;
+            input.innerText = addSymbol(el.innerText, input.innerText);
             break;
         case "percent":
         case "period":
-            if (input.innerText.match(/\d$/)) input.innerText += el.innerText;
+            input.innerText += addSecondary(el.innerText, input.innerText);
             break;
         case "swapsign":
             if (!input.innerText.match(/(\d|%)$/)) break;
@@ -38,9 +70,21 @@ document.addEventListener("click", (ev) => {
             if (lastitem.startsWith("-")) temp[temp.length - 1] = temp[temp.length - 1].substring(1);
             else temp[temp.length - 1] = "-" + temp[temp.length - 1]
             input.innerText = temp.join(" ");
-        case "erase": 
-        if (input.innerText.match(/(\d|%)$/)) 
-            input.innerText = input.innerText.substring(0, input.innerText.length - 1)
-        else input.innerText = input.innerText.substring(0, input.innerText.length - 2)
+        case "erase":
+            input.innerText = erase(input.innerText);
+            break;
+        case "equals":
+            input.innerText = compute(input.innerText)
+            break;
     }
 })
+
+document.addEventListener('keydown', (e) => {
+    const input = document.getElementById("input")
+    if (e.key.match(/\d/)) input.innerText = addNumber(e.key, input.innerText);
+    else if (["+", "-", "*"].includes(e.key)) input.innerText = addSymbol(e.key, input.innerText)
+    else if (e.key === "/") input.innerText = addSymbol("÷", input.innerText)
+    else if (["%", "."].includes(e.key)) input.innerText = addSecondary(e.key, input.innerText)
+    else if (e.key === "Backspace") input.innerText = erase(input.innerText)
+    else if (e.key === "Enter") input.innerText = compute(input.innerText)
+});
